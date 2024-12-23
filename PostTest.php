8fdbe1eb16b3837a8e04490e884b826318be92c4
farhandwp <20230910110@uniku.ac.id>
2024-12-23 21:16:27 +0700

    Modul 8

---
+++ PostTest.php
@@ -0,0 +1,13 @@
+<?php
+$a = 5;
+$b = 10;
+
+// Menghitung penjumlahan
+$sum = $a + $b;
+
+// Menggunakan tanda kutip ganda
+echo "Hasil penjumlahan $a + $b adalah $sum.\n";
+
+// Menggunakan tanda kutip tunggal dengan operator titik
+echo 'Hasil penjumlahan ' . $a . ' + ' . $b . ' adalah ' . $sum . ".\n";
+?>
